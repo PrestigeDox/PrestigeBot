bdff19c878861d9c80878f6df2b9c48942ca3fda
PrestigeDox <themutantreaper@gmail.com>
2017-10-16 15:44:57 +0100

    Working welcome and leave messages

--- welcome.py
+++ welcome.py
@@ -14,6 +14,12 @@ class Name:
 		emb.add_field(name='$welcome check', value='Check if your server is setup/see the welcome message', inline=False)
 		await ctx.send(embed=emb)
 
+	@commands.group(invoke_without_command=True, pass_context=True)
+	async def leave(self, ctx):
+		emb = discord.Embed(title='\U00002709 Welcome Help', colour=0xC500FF)
+		emb.add_field(name='$leave setmessage', value='Setup the help command on your server/also use to change the current welcome message', inline=False)
+		await ctx.send(embed=emb)
+
 	@welcome.command(hidden=True)
 	async def check(self, ctx):
 		holder = {}
@@ -29,59 +35,67 @@ class Name:
 			await ctx.send(embed=emb)
 
 	@welcome.command(hidden=True)
+	@commands.has_permissions(administrator=True)
 	async def setmessage(self, ctx, *, welcome_message: str = None):
 		holder = {}
 		with open('welcome.json', 'r') as f:
 			holder = json.load(f)
 		if welcome_message == None:
 			embd = discord.Embed(colour=0xff0c00)
-			embd.add_field(name='\U0000274c Error', value='Welcome message is required')
+			embd.add_field(name='\U0000274c Error', value='Welcome_message is required')
 			await ctx.send(embed=embd)
-		elif '{}' not in welcome_message:
+		elif "{}" not in welcome_message:
 			embd = discord.Embed(colour=0xff0c00)
-			embd.add_field(name='\U0000274c Error', value='Welcome message must contain {} for user mention')
+			embd.add_field(name='\U0000274c Error', value='{} is required within the welcome message to show a user mention')
 			await ctx.send(embed=embd)
 		else:
-			holder[str(ctx.guild.id)][1] = welcome_message
+			holder[str(ctx.guild.id)]["welcomemessage"] = welcome_message
 			with open('welcome.json', 'w') as f:
-				holder = json.load(f)
+				json.dump(holder, f)
 			emb = discord.Embed(colour=0xC500FF)
-			emb.add_field(name='Message', value='Message Updated', inline=False)
+			emb.add_field(name='Status', value='New message has been saved!', inline=False)
+			await ctx.send(embed=emb)
+
+	@leave.command(hidden=True)
+	@commands.has_permissions(administrator=True)
+	async def setmessage(self, ctx, *, leave_message: str = None):
+		holder = {}
+		with open('welcome.json', 'r') as f:
+			holder = json.load(f)
+		if leave_message == None:
+			embd = discord.Embed(colour=0xff0c00)
+			embd.add_field(name='\U0000274c Error', value='Leave_message is required')
+			await ctx.send(embed=embd)
+		elif "{}" not in leave_message:
+			embd = discord.Embed(colour=0xff0c00)
+			embd.add_field(name='\U0000274c Error', value='{} is required within the welcome message to show a user mention')
+			await ctx.send(embed=embd)
+		else:
+			holder[str(ctx.guild.id)]["leavemessage"] = leave_message
+			with open('welcome.json', 'w') as f:
+				json.dump(holder, f)
+			emb = discord.Embed(colour=0xC500FF)
+			emb.add_field(name='Status', value='New message has been saved!', inline=False)
 			await ctx.send(embed=emb)
 
 	@welcome.command(hidden=True)
+	@commands.has_permissions(administrator=True)
 	async def setchannel(self, ctx, *, welcome_channel: discord.TextChannel = None):
 		holder = {}
 		with open('welcome.json', 'r') as f:
 			holder = json.load(f)
 		if welcome_channel == None:
 			embd = discord.Embed(colour=0xff0c00)
-			embd.add_field(name='\U0000274c Error', value='Welcome channel is required')
+			embd.add_field(name='\U0000274c Error', value='Welcome_channel is required')
 			await ctx.send(embed=embd)
-		elif str(ctx.guild.id) not in holder:
-			print("1) Start of ElIf")
-			holder[str(ctx.guild.id)] = []
-			print("2) Add server to dict")
-			holder[str(ctx.guild.id)][0] = welcome_channel.id
-			print("3) Give server channel ID")
-			channelid = welcome_channel.id
+		else:
+			holder[str(ctx.guild.id)]["channelid"] = welcome_channel.id
 			with open('welcome.json', 'w') as f:
 				json.dump(holder, f)
-			print("4) Write to file")
 			emb = discord.Embed(colour=0xC500FF)
-			emb.add_field(name='Message', value=message, inline=False)
-			emb.add_field(name='Channel', value=str(channelid), inline=False)
+			emb.add_field(name='Status', value='New channel has been saved!', inline=False)
 			await ctx.send(embed=emb)
-			print("5) Send message")
-		else:
-			holder[str(ctx.guild.id)][0] = welcome_channel.id
-			channelid = welcome_channel.id
-			with open('welcome.json', 'w') as f:
-				holder = json.load(f)
-			emb = discord.Embed(colour=0xC500FF)
-			emb.add_field(name='Message', value=message, inline=False)
-			emb.add_field(name='Channel', value=str(channelid), inline=False)
-			await ctx.send(embed=emb)
+		
 
 def setup(bot):
 	bot.add_cog(Name(bot))
